965c61be732e3cddf710511a334698e9a8a37c06
Igor Zhovtaniuk <igorzhovtyj@gmail.com>
2017-03-17 16:28:32 +0200

    OrderShippedMail

--- resources/views/email/newOrder.blade.php
+++ resources/views/email/newOrder.blade.php
@@ -42,7 +42,8 @@
             @foreach($order->orderServices as $orderService)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
-                    <td><img src="{{url()}}/upload_images/services/{{ $orderService->service->image }}" style="width:250px;"></td>
+                    <td><img src="http://agency.irank.website/upload_images/services/{{ $orderService->service->image }}" style="width:250px;"></td>
+
                     <td><strong>{{ $orderService->service->name }}</strong></td>
                     <td>${{ $orderService->price }}</td>
                     <td>
