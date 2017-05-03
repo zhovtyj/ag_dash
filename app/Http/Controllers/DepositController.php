8fe9b06948fe28625108d3b44581f61c3ad696cf
Igor Zhovtaniuk <igorzhovtyj@gmail.com>
2017-05-03 19:32:47 +0300

    PayPal Live. Fix 4. DepositController

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -34,8 +34,8 @@ class DepositController extends Controller
             config('services.paypal.secret'));
 
         $this->_apiContext->setConfig(array(
-            'mode' => 'sandbox',
-            'service.EndPoint' => 'https://api.sandbox.paypal.com',
+            'mode' => 'production',
+            'service.EndPoint' => 'https://api.paypal.com/',
             'http.ConnectionTimeOut' => 30,
             'log.LogEnabled' => true,
             'log.FileName' => storage_path('logs/paypal.log'),
