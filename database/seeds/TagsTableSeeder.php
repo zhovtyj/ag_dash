675d7abdc4459a80227ff7213fc82bc335d9f3a6
Igor Zhovtaniuk <igorzhovtyj@gmail.com>
2017-05-03 19:50:37 +0300

    Tags Seeder

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -13,20 +13,44 @@ class TagsTableSeeder extends Seeder
     public function run()
     {
         $tag1 = new Tag();
-        $tag1->name = 'wholesale';
+        $tag1->name = 'Urgent';
         $tag1->save();
 
         $tag2 = new Tag();
-        $tag2->name = 'mmbb';
+        $tag2->name = 'Local SEO';
         $tag2->save();
 
         $tag3 = new Tag();
-        $tag3->name = 'whitelabel';
+        $tag3->name = 'National SEO';
         $tag3->save();
 
         $tag4 = new Tag();
-        $tag4->name = 'irank';
+        $tag4->name = 'Link Building';
         $tag4->save();
 
+        $tag4 = new Tag();
+        $tag4->name = 'GMB Optimization';
+        $tag4->save();
+
+        $tag4 = new Tag();
+        $tag4->name = 'Reputation Mgmnt';
+        $tag4->save();
+
+        $tag4 = new Tag();
+        $tag4->name = 'Biz Listing Mgmnt';
+        $tag4->save();
+
+        $tag4 = new Tag();
+        $tag4->name = 'Social Media Mgmnt';
+        $tag4->save();
+
+        $tag4 = new Tag();
+        $tag4->name = 'Content and PR News';
+        $tag4->save();
+
+        $tag4 = new Tag();
+        $tag4->name = 'PPC and Paid Traffic';
+        $tag4->save();
+        
     }
 }
